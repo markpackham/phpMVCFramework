f2b59b15b0b30997c3193bfbc8301ba790a8f3f7
mark <markpackham1@gmail.com>
2021-11-29 13:11:26 +0000

    get url words
    https://youtu.be/n2yeK6LwSII?t=1218

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,28 @@
 <?php
+
+class Core
+{
+    protected $currentController = "Pages";
+    protected $currentMethod = "index";
+    // holds key parts of url
+    // eg http://example.com/shop/glasses/men
+    protected $params = [];
+
+    public function __construct()
+    {
+        // print_r($this->getUrl());
+    }
+
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], "/");
+
+            // forbid characters that the url shouldn't have eg @ or $
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // break into an array
+            $url = explode("/", $url);
+            return $url;
+        }
+    }
+}
--- app/require.php
+++ app/require.php
@@ -4,3 +4,6 @@ require_once 'libraries/Core.php';
 require_once 'libraries/Controller.php';
 require_once 'libraries/Database.php';
 require_once 'config/config.php';
+
+// Instantiate Core class
+$init = new Core();
