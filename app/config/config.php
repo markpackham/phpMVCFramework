a4b3f069a23933d8bf249ba1b48f3d473d7ecf04
mark <markpackham1@gmail.com>
2021-11-29 19:42:57 +0000

    setup database config
    https://youtu.be/n2yeK6LwSII?t=3229

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 // Databse params
-define("DB_HOST", "");
-define("DB_USER", "");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "");
+define("DB_NAME", "phpmvcframework");
 
 // shows something like
 //C:\xampp\htdocs\phpMVCFramework\app\config\config.php
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,1 +1,29 @@
 <?php
+class Database
+{
+    private $dbHost = DB_HOST;
+    private $dbUser = DB_USER;
+    private $dbPass = DB_PASS;
+    private $dbName = DB_NAME;
+
+    private $statement;
+    private $dbHandler;
+    private $error;
+
+    public function __construct()
+    {
+        $conn = "mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName;
+        $options = array(
+            // prevent driver timeouts when connecting to database
+            // also check if a connection has already been established
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        );
+        try {
+            $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+}
